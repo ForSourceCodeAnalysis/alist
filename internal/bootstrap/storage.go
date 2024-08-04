83b7e6751f5dc43b2c3a12c49070025b1f23a77f
ForSourceCodeAnalysis <sourceCodeAnalysis@aliyun.com>
2024-08-04 06:50:04 +0000

    feat:auto backup

--- internal/bootstrap/storage.go
+++ internal/bootstrap/storage.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/alist-org/alist/v3/internal/conf"
 	"github.com/alist-org/alist/v3/internal/db"
-	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/alist-org/alist/v3/internal/op"
 	"github.com/alist-org/alist/v3/pkg/utils"
 )
@@ -15,16 +14,15 @@ func LoadStorages() {
 	if err != nil {
 		utils.Log.Fatalf("failed get enabled storages: %+v", err)
 	}
-	go func(storages []model.Storage) {
-		for i := range storages {
-			err := op.LoadStorage(context.Background(), storages[i])
-			if err != nil {
-				utils.Log.Errorf("failed get enabled storages: %+v", err)
-			} else {
-				utils.Log.Infof("success load storage: [%s], driver: [%s], order: [%d]",
-					storages[i].MountPath, storages[i].Driver, storages[i].Order)
-			}
+
+	for i := range storages {
+		err := op.LoadStorage(context.Background(), storages[i])
+		if err != nil {
+			utils.Log.Errorf("failed get enabled storages: %+v", err)
+		} else {
+			utils.Log.Infof("success load storage: [%s], driver: [%s], order: [%d]",
+				storages[i].MountPath, storages[i].Driver, storages[i].Order)
 		}
-		conf.StoragesLoaded = true
-	}(storages)
+	}
+	conf.StoragesLoaded = true
 }
