f50598de183ba53d69d0ae29eb93d695acd9bf1e
ForSourceCodeAnalysis <sourceCodeAnalysis@aliyun.com>
2024-10-25 07:28:50 +0000

    添加备案信息配置项

--- internal/bootstrap/data/setting.go
+++ internal/bootstrap/data/setting.go
@@ -91,6 +91,9 @@ func InitialSettings() []model.SettingItem {
 		{Key: conf.AllowIndexed, Value: "false", Type: conf.TypeBool, Group: model.SITE},
 		{Key: conf.AllowMounted, Value: "true", Type: conf.TypeBool, Group: model.SITE},
 		{Key: conf.RobotsTxt, Value: "User-agent: *\nAllow: /", Type: conf.TypeText, Group: model.SITE},
+		{Key: conf.FilingNumber, Value: "", Type: conf.TypeString, Group: model.SITE},
+		{Key: conf.FilingUrl, Value: "", Type: conf.TypeString, Group: model.SITE},
+
 		// style settings
 		{Key: conf.Logo, Value: "https://cdn.jsdelivr.net/gh/alist-org/logo@main/logo.svg", Type: conf.TypeText, Group: model.STYLE},
 		{Key: conf.Favicon, Value: "https://cdn.jsdelivr.net/gh/alist-org/logo@main/logo.svg", Type: conf.TypeString, Group: model.STYLE},
