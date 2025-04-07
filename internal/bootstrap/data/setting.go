db9a6641a56f5dddf40601c52a9483f633c3bcdc
ForSourceCodeAnalysis <sourceCodeAnalysis@aliyun.com>
2025-04-07 11:56:27 +0800

    feat(config):add filling config

--- internal/bootstrap/data/setting.go
+++ internal/bootstrap/data/setting.go
@@ -93,7 +93,6 @@ func InitialSettings() []model.SettingItem {
 		{Key: conf.RobotsTxt, Value: "User-agent: *\nAllow: /", Type: conf.TypeText, Group: model.SITE},
 		{Key: conf.FilingNumber, Value: "", Type: conf.TypeString, Group: model.SITE},
 		{Key: conf.FilingUrl, Value: "", Type: conf.TypeString, Group: model.SITE},
-
 		// style settings
 		{Key: conf.Logo, Value: "https://cdn.jsdelivr.net/gh/alist-org/logo@main/logo.svg", Type: conf.TypeText, Group: model.STYLE},
 		{Key: conf.Favicon, Value: "https://cdn.jsdelivr.net/gh/alist-org/logo@main/logo.svg", Type: conf.TypeString, Group: model.STYLE},
